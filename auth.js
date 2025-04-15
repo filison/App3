7b515414b8f0aea6eae7f1d7fff9bab2583a66c7
filison <65769401+filison@users.noreply.github.com>
2025-04-15 16:05:11 +0200

    auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "TVŮJ_API_KLÍČ",
-  authDomain: "TVŮJ_AUTH_DOMAIN",
-  projectId: "TVŮJ_PROJECT_ID",
-  storageBucket: "TVŮJ_BUCKET",
-  messagingSenderId: "TVŮJ_SENDER_ID",
-  appId: "TVŮJ_APP_ID"
+  apiKey: "AIzaSyAcg0qqzWJwkxmopkTgzfasMqd3siQNxIU",
+  authDomain: "zvladnuto-4e6cb.firebaseapp.com",
+  projectId: "zvladnuto-4e6cb",
+  storageBucket: "zvladnuto-4e6cb.firebasestorage.app",
+  messagingSenderId: "299881468872",
+  appId: "1:299881468872:web:8c36e215cf40a91748e6fd",
+  measurementId: "G-HZZG7JRYCC"
 };
 
 firebase.initializeApp(firebaseConfig);
